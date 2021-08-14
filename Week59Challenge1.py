10fb2b21e725f5f71c9668bbe2f168dd7f590828
Iliya-Yadegari <naruto3@email.com>
2021-08-14 20:18:06 +0100

    Create Week59Challenge1.py

---
+++ Week59Challenge1.py
@@ -0,0 +1,43 @@
+def calculateCharges(c1,c2,c3,c4,c5):
+    c1 -= 3
+    c2 -= 3
+    c3 -= 3
+    c4 -= 3
+    c5 -= 3
+    
+    if c1 <= 0:
+        print('You have no charge to pay')
+    else:
+        price = c1 * 0.5
+        
+        print(price)
+        
+    if c2 <= 0:
+        print('You have no charge to pay')
+    else:    
+        price = c1 * 0.5
+        
+        print(price)        
+        
+    if c3 <= 0:
+        print('You have no charge to pay')
+    else:
+        price = c1 * 0.5
+        
+        print(price)
+    
+    if c4 <= 0:
+        print('You have no charge to pay')
+        
+    else:
+        price = c1 * 0.5
+        
+        print(price)
+        
+    if c5 <= 0:
+        print('You have no charge to pay')
+        
+    else:
+        price = c1 * 0.5
+        
+        print(price)
